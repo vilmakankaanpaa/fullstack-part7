f7a7339de3f7db8d84d0e5a269cdf24624855330
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-11-16 15:01:00 +0200

    Exercise 7.4

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -6,6 +6,7 @@ import {
   useMatch,
   useNavigate
 } from 'react-router-dom'
+import  { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -80,22 +81,22 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/anecdotes')
-    props.viewMessage(`a new anecdote ${content} created`)
+    props.viewMessage(`a new anecdote ${content.value} created`)
   }
 
   return (
@@ -104,15 +105,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -176,10 +177,10 @@ const App = () => {
       <Menu />
       <Notification message={notification}/>
       <Routes>
-        <Route path="anecdotes/:id" element={<Anecdote anecdote={anecdote}/>} />
-        <Route path="anecdotes" element={<AnecdoteList anecdotes={anecdotes} />} />
-        <Route path="about" element={<About />} />
-        <Route path="create" element={<CreateNew addNew={addNew} viewMessage={viewMessage}/>} />
+        <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote}/>} />
+        <Route path="/anecdotes" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create" element={<CreateNew addNew={addNew} viewMessage={viewMessage}/>} />
         <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
       </Routes>
       <Footer />
